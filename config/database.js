5fe7522ab3f8bdf9315a4d9146686fbc713b6a41
Maxime Wets <wetsmaxime@gmail.com>
2023-03-27 06:23:46 +0200

    🔧 removed env section + added DB SSL certificate

--- config/database.js
+++ config/database.js
@@ -1,15 +1,21 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
 module.exports = ({ env }) => ({
-  connection: {    
-      client: env('DATABASE_CLIENT', 'postgres'),
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'jbb'),
-        user: env('DATABASE_USERNAME', 'jbbeauty'),
-        password: env('DATABASE_PASSWORD', 'jbbeauty'),
-        },
-        debug: false,
+  connection: {
+    client: env("DATABASE_CLIENT", "postgres"),
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "jbb"),
+      user: env("DATABASE_USERNAME", "jbbeauty"),
+      password: env("DATABASE_PASSWORD", "jbbeauty"),
     },
+    ssl: {
+      ca: fs
+        .readFileSync(`${__dirname}/certificates/DigiCertGlobalRootCA.crt.pem`)
+        .toString(),
+    },
+    debug: false,
+  },
 });
