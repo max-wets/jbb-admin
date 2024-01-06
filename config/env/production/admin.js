ea785bf7d01253af4af6532c33e912e90a386030
Maxime Wets <wetsmaxime@gmail.com>
2024-01-06 16:27:47 +0100

    Added API token salt + removed public URL

--- config/env/production/admin.js
+++ config/env/production/admin.js
@@ -1,8 +1,10 @@
+const crypto = require("crypto");
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env("API_TOKEN_SALT", "5c8d02f52bfd5896f99e89b57b159073"),
+    salt: env("API_TOKEN_SALT", crypto.randomBytes(16).toString("base64")),
   },
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,5 +4,4 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "jbb-admin.azurewebsites.net"),
 });
